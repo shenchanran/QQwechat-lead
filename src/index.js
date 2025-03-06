51fdc4c8d433011c10e7c73636fe1a2a3bc14cc2
shenchanran <14284537@qq.com>
2025-03-06 14:55:52 +0800

    优化跳转显示

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import QQicon from './icon-qq.png'
 const UA = navigator.userAgent.toLowerCase()
 const $ = e => { return document.querySelector(e) }
 $('.arrow').src = arrow
+document.title = '请在浏览器打开'
 if (UA.includes('micromessenger')) {
     $('.icon').src = WXicon
 } else if (UA.includes('qqbrowser')) {
     $('.icon').src = QQicon
 } else {
+    document.title = '跳转中'
     const params = new URLSearchParams(window.location.search);
     const s = document.createElement('script')
     s.src = 'https://example.com/?k='+params.get('k')
     $('head').appendChild(s)
+    document.body.innerHTML = ''
 }
-document.title = '请在浏览器打开'
